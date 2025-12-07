2022525960a057f1581236c0e03879db0ab3f529
erkinboyev islom <erkinboyev@kali.Erkinboyev>
2025-12-07 21:12:35 +0500

    Update bot with images and inline buttons

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Telegraf, Markup } = require('telegraf');
-const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
+const bot = new Telegraf("8446312657:AAEvVNtCgakIMFOt7YTy_yPYqDxRvjLLOqE");
 bot.start((ctx) => {ctx.reply(`Salom ${ctx.from.first_name}! Botimizga xush kelibsiz.`, Markup.inlineKeyboard([[Markup.button.callback('Rasm ko‘rish','SHOW_IMAGE')],[Markup.button.callback('E’lonlar','SHOW_ADS')]]))});
 bot.action('SHOW_IMAGE',(ctx)=>{ctx.replyWithPhoto({url:'https://picsum.photos/400/300'},{caption:'Mana siz uchun rasm!'})});
 bot.action('SHOW_ADS',(ctx)=>{ctx.reply('📝 Bugungi e’lon: Sizga maxsus takliflar mavjud!')});
